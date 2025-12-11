83de7e2f9bbe209bdede831bf4cf0a2f9b0ff3cc
bdsriver <bendachuan@gmail.com>
2025-12-11 14:45:14 -0600

    added tests for all functions

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,10 +3,40 @@ from pathlib import Path
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
-from app import add
+from app import *
 
 def test_add():
     assert add(5,6) == 11
 
 def test_add2():
     assert add(5,6) != 10
+
+def test_mul():
+    assert multiply(6,10) == 60
+
+def test_mul2():
+    assert multiply(3,4) != 10
+
+def test_sub():
+    assert subtract(5,4) == 1
+
+def test_div():
+    assert divide(10,2) == 5
+
+def test_sq():
+    assert square(4) == 16
+
+def test_log():
+    assert log(1) == 0
+
+def test_sin():
+    assert sin(0) == 0
+
+def test_cos():
+    assert cos(0) == 1
+
+def test_sqrt():
+    assert square_root(25) == 5
+
+def test_percentage():
+    assert percentage(10,4) == 40
